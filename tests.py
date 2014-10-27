8048a6534450ece0059b4891cb236dc727fed82e
tino415 <cernakmartin4@gmail.com>
2014-10-27 16:34:50 +0100

    Added generation of unauthorized packet if neceseary

--- tests.py
+++ tests.py
@@ -14,3 +14,26 @@ def generate_digest():
 def generate_via():
 	reload(SIP)
 	server = SIP.Server()
+
+def get_top_most_via(packet_path = "packets/register1-s1.sip"):
+	reload(SIP)
+	packet_str =  ''.join(open(packet_path).readlines())
+	packet = SIP.parse_packet(packet_str)
+	print packet.get_return_address()
+
+def register_action_1(packet_path = "packets/register1-s1.sip"):
+	reload(SIP)
+	packet_str = ''.join(open(packet_path).readlines())
+	server = SIP.Server()
+	packet = SIP.parse_packet(packet_str)
+	print "STARTING TEST\n"
+	server.register_action(packet)
+
+def register_action_2(packet_path = "packets/register2-s1.sip"):
+	reload(SIP)
+	packet_str = ''.join(open(packet_path).readlines())
+	server = SIP.Server()
+	packet = SIP.parse_packet(packet_str)
+	print "STARTING TEST\n"
+	server.register_action(packet)
+	
